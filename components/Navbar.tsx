a8060ce4c9dc9f96c8e1e2c1b9e11fc460922feb
Rounak-28 <rounakkumbhakar2004@gmail.com>
2023-01-15 16:51:17 +0530

    added comment feature

--- components/Navbar.tsx
+++ components/Navbar.tsx
@@ -36,7 +36,6 @@ const Navbar = ({ session }: any) => {
         <img
           src={session?.user?.image}
           className="w-9 h-9 rounded-full absolute right-4 top-2"
-          onClick={()=> signOut()}
         />
         <div className="line w-full bg-[#00000036] h-[1px] absolute bottom-0"></div>
       </div>
